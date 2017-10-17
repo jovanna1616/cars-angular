dec7f9019c2f03960b75c31f6f401032df1b98b6
Vivify Academy <ladybutterfly.fly@gmail.com>
2017-10-17 18:46:11 +0200

    prvi zadatak-Bootstrap+CarsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { CarsComponent } from './components/cars/cars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    CarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  exports: [
+  	LayoutComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
